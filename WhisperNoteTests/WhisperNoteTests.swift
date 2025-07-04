0ed6d5646c8a7a3dbb286bd00501e6038af3805b
Kirlos Yousef <kirlosek@gmail.com>
2025-07-04 13:46:55 +0300

    Initial Commit

---
+++ WhisperNoteTests/WhisperNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperNoteTests.swift
+//  WhisperNoteTests
+//
+//  Created by Kirlos Yousef on 04/07/2025.
+//
+
+import Testing
+@testable import WhisperNote
+
+struct WhisperNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
